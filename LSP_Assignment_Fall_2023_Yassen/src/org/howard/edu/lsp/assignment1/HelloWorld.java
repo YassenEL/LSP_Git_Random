4707c515a043d984378da69d632ec3f6da932e9c
YassenEL <yassen.elshaikh@bison.howard.edu>
2023-08-24 20:00:44 -0400

    I hope this works

---
+++ LSP_Assignment_Fall_2023_Yassen/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	/**
+	 * Yassen Elshaikh
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
