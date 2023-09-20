8d0bd950387ce23386b854eb8b9cb7f4ea3a3588
YassenEL <yassen.elshaikh@bison.howard.edu>
2023-09-20 11:22:06 -0400

    added comments

--- LSP_Assignment_Fall_2023_Yassen/src/org/howard/edu/lsp/assignment2/Assignment_2.java
+++ LSP_Assignment_Fall_2023_Yassen/src/org/howard/edu/lsp/assignment2/Assignment_2.java
@@ -6,27 +6,27 @@ import java.util.HashMap;
 public class Assignment_2 {
 
 	public static void main(String[] args) {
-		FileReader filename = new FileReader();
+		FileReader filename = new FileReader(); //Uses The FileReader class to read the file
 		HashMap<String, Integer> hashmap = new HashMap<String, Integer>();
 		try {
-			String filepath = filename.readToString("main/java/resources/words.txt");
+			String filepath = filename.readToString("main/java/resources/words.txt");// FilePath 
 			System.out.println(filepath);
-			String[] seperated = filepath.toLowerCase().split(" ");
-			for (int x = 0; x < seperated.length; x++) {
-				if (seperated[x].length() > 3) {
+			String[] seperated = filepath.toLowerCase().split(" ");//This removes all non-characters such as spaces
+			for (int x = 0; x < seperated.length; x++) {// Separating the file itself
+				if (seperated[x].length() > 3) { 
 					if (hashmap.get(seperated[x]) == null) {
-						hashmap.put(seperated[x], 1);
+						hashmap.put(seperated[x], 1); //setting number of occurrences to 1
 					}
 					else {
-						hashmap.put(seperated[x], hashmap.get(seperated[x]) + 1);
+						hashmap.put(seperated[x], hashmap.get(seperated[x]) + 1);//increasing number of times it occurs
 					}
 				}
 			}
-			for (String i : hashmap.keySet()) {
+			for (String i : hashmap.keySet()) {// printing each hashmap key
 				System.out.println(i + " " + hashmap.get(i));
 			}
 		}
-		catch (FileNotFoundException e) {
+		catch (FileNotFoundException e) {//Catchers any error's or exceptions found
 			System.out.println("File not found in directory");
 		}
 
