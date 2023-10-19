a57221592d92b67c78530f178f9e092d60db157a
YassenEL <yassen.elshaikh@bison.howard.edu>
2023-10-19 19:35:56 -0400

    Completed the code for the book function as well as the driver code

--- LSP_Assignment_Fall_2023_Yassen/src/org/howard/edu/lsp/midterm/problem1/Book.java
+++ LSP_Assignment_Fall_2023_Yassen/src/org/howard/edu/lsp/midterm/problem1/Book.java
@@ -1,5 +1,34 @@
 package org.howard.edu.lsp.midterm.problem1;
 
 public class Book {
+	//Creating the class attributes
+    private String title;
+    private String author;
+    private int year;
 
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    //Overriding the equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Book otherBook = (Book) obj;
+        return title.equals(otherBook.title) && author.equals(otherBook.author);
+    }
+
+   //Overriding the toString method
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Year: " + year;
+    }
 }
+
